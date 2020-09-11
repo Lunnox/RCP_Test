d4fb3bc41cbdb71eb4b94a91a48658779a8bcb8d
lunnox <fire.lunnox@gmail.com>
2020-09-11 18:42:19 +0300

    Tests: create simple demo test with SWTBot

---
+++ tests/com.vogella.tycho.rcp.tests/src/com/vogella/tycho/rcp/tests/ExampleTest.java
@@ -0,0 +1,15 @@
+package com.vogella.tycho.rcp.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        // just an example
+        assertTrue(true);
+    }
+
+}
